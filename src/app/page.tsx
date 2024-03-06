28ccf99dc0fa188fbca1d2d5351ee35f0e80ef24
Agustin Rubilar D. <agnrubilar@gmail.com>
2024-03-06 16:37:38 -0300

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>CI/CD GithubActions test</h1>
+      <h1>CI/CD GithubActions test1</h1>
     </main>
   ); 
 }  
