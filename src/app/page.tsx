a13b6a95874e5c6bba6600d32006e944f3becc8d
Knightkoder <agnrubilar@gmail.com>
2024-03-07 00:45:12 -0300

    ga1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>CI/CD GithubActions test2</h1> 
+      <div>Prueba subida1</div>
       <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
     </main>
   );  
