deb7544691f85c7bf13916f08494304f3a680f30
Knightkoder <agnrubilar@gmail.com>
2024-03-08 16:05:18 -0300

    nextimage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import Nextimage from "next/image";
+
+
 
 export default function Home() {
-  return (
+  return (<>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>CI/CD GithubActions test2</h1> 
       <div className="flex flex-col items-center justify-center">
@@ -15,7 +18,12 @@ export default function Home() {
           Learn Next.js
         </a>
         </div>
-
+        <Nextimage
+          src="https://nextjs.org/static/images/nextjs-black-logo.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={60}
+        />
       <section className="flex flex-col items-center justify-center">
           <h2 className="text-2xl font-bold">
             Welcome to <a href="https://nextjs.org">Next.js!</a>
@@ -23,7 +31,7 @@ export default function Home() {
       </section>
       <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
     </main>
-  );  
+  </>);  
 }  
 
 
